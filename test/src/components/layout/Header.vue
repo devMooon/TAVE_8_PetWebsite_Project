1f4c3446d7105a799be86b4bda8884a4c9e6f179
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-10-30 17:31:03 +0900

    navbar 수정

--- test/src/components/layout/Header.vue
+++ test/src/components/layout/Header.vue
@@ -1,13 +1,12 @@
 <template>
     <!-- navbar -->
-    <b-navbar class="fixed-top bg-white topbar mb-4 static-top shadow" toggleable="lg" type="light" style="background-color: #f3f3f3;">
+        <b-navbar class="fixed-top bg-white topbar mb-4 static-top" toggleable="lg" type="light" style="background-color: #f3f3f3;">
         <b-container class="bv-example-row">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+            <b-navbar-brand href="/">멍냥</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/">Home</b-nav-item>
                     <b-nav-item-dropdown text="예약하기" right>
                         <b-dropdown-item href="#"><i class="far fa-hospital"></i>&nbsp;&nbsp; 병원</b-dropdown-item>
                         <b-dropdown-item href="#"><i class="far fa-user"></i>&nbsp;&nbsp; 미용실</b-dropdown-item>
@@ -27,10 +26,12 @@
                         <b-dropdown-item href="#">내가 쓴 댓글</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
-                    <b-form-input size="sm" placeholder="Search"></b-form-input>
-                    <b-button size="sm" type="submit"><font-awesome-icon icon="search" /></b-button>
-                    <a class="nav-link btn btn-outline-secondary btn-sm" href="/Login" style="font-size:10px">로그인</a>
-                    <a class="nav-link btn btn-outline-secondary btn-sm" href="/Signup" style="font-size:10px">회원가입</a>           
+                <b-navbar-nav class="ml-5">
+                    <span><b-form-input size="sm" class="ml-2" placeholder="Search"></b-form-input></span>
+                    <span><b-button size="sm" type="submit"><font-awesome-icon icon="search" /></b-button></span>
+                    <span><b-button size="sm" type="submit" variant="outline-secondary" href="/Login" style="font-size:10px">로그인</b-button></span>
+                    <span><b-button size="sm" type="submit" variant="outline-secondary" href="/Signup" style="font-size:10px">회원가입</b-button></span>
+                </b-navbar-nav>
             </b-collapse>
         </b-container>
     </b-navbar>
