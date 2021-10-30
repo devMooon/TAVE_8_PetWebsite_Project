5abed4b901ba752316737c5537d95bc69c1f413a
Seo-Yeon Mun <anstjdus0702@duksung.ac.kr>
2021-10-30 13:54:30 +0900

    font awesome import

--- test/src/main.js
+++ test/src/main.js
@@ -1,18 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import {
   BootstrapVue,
   IconsPlugin
 } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+library.add(
+  faUserSecret,
+  faSearch,
+)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
